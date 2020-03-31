bee2d60c210befc4801f29d17c458e779f8b4312
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-03-31 10:56:39 +0530

    update shivneri and related changes

--- chat/src/controllers/chat_controller.cr
+++ chat/src/controllers/chat_controller.cr
@@ -1,19 +1,19 @@
 module Chat
   module Controller
     class ChatController < Shivneri::WebSocketController
-      @[Event]
+      @[On("join-group")]
       def join_group(group_name : String)
         # add current client to a group
         clients.groups.add(group_name)
 
         # send message to caller
-        clients.current.emit("message", "Welcome to the group")
+        clients.current.emit("message", "Welcome to the group #{group_name}")
       end
 
-      @[Event]
+      @[On("big-bang")]
       def to_big_bang(data : NamedTuple(from: String, message: String))
         # send message to a group except caller
-        clients.groups.except_me("big_bang").emit("from_group_big_bang", data)
+        clients.groups.except_me("big-bang").emit("big-bang", data)
       end
 
       def connected
